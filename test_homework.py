e0780b555a530c9e36f33553a1151b190a2704a7
Nataly Isaeva <mamadany25@gmail.com>
2024-08-12 08:07:21 +0300

    done

---
+++ test_homework.py
@@ -0,0 +1,83 @@
+import math
+import random
+
+
+def test_greeting():
+    """
+    Напишите программу, которая выводит на экран приветствие.
+    """
+    name = "Наталья"
+    age = 34
+    output = f"Привет, {name}! Тебе {age} года."
+    print(output)
+    # Проверяем результат
+    assert output == "Привет, Наталья! Тебе 34 года."
+
+
+def test_rectangle():
+    """
+    Напишите программу, которая берет длину и ширину прямоугольника
+    и считает его периметр и площадь.
+    """
+    a = 20
+    b = 30
+    perimeter = 2 * (a + b)
+
+    assert perimeter == 100
+
+    area = a * b
+
+    assert area == 600
+
+
+def test_circle():
+    """
+    Напишите программу, которая берет радиус круга и выводит на экран его длину и площадь.
+    Используйте константу PI
+    """
+    r = 23
+    area = math.pi * (r ** 2)
+    print(area)
+    assert area == 1661.9025137490005
+
+    length = 2 * math.pi * r
+    print(length)
+    assert length == 144.51326206513048
+
+
+def test_random_list():
+    """
+    Создайте список из 10 случайных чисел от 1 до 100 (включая обе границы) и отсортируйте его по возрастанию.
+    """
+    l = [random.randint(1, 100) for _ in range(10)]
+    l.sort()
+    assert len(l) == 10
+    assert all(l[i] <= l[i + 1] for i in range(len(l) - 1))
+
+
+def test_unique_elements():
+    """
+    Удалите из списка все повторяющиеся элементы
+    """
+    l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
+    l = sorted(set(l))
+
+    assert isinstance(l, list)
+    assert len(l) == 10
+    assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def test_dicts():
+    """
+    Создайте словарь из двух списков.
+    Используйте первый список как ключи, а второй - как значения.
+    Подсказка: используйте встроенную функцию zip.
+    """
+    first = ["a", "b", "c", "d", "e"]
+    second = [1, 2, 3, 4, 5]
+    d = dict(zip(first, second))
+
+    assert isinstance(d, dict)
+    assert len(d) == 5
+    assert list(d.keys()) == first
+    assert list(d.values()) == second
